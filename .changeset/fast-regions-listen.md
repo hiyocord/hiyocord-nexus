aa03fd40acd3ceecd1c83fe9b1b8c11c06b4f4ab
kurages <contact@kurages.dev>
2026-03-27 11:14:26 +0000

    discord api proxy用のmiddlewareを実装

---
+++ .changeset/fast-regions-listen.md
@@ -0,0 +1,6 @@
+---
+"@hiyocord/hiyocord-nexus-types": patch
+"@hiyocord/hiyocord-nexus-core": patch
+---
+
+discord api proxy 用の middleware を実装
--- packages/hiyocord-nexus-core/src/middlewares/discord-client.ts
+++ packages/hiyocord-nexus-core/src/middlewares/discord-client.ts
@@ -1,7 +1,9 @@
-import { paths } from "@hiyocord/hiyocord-nexus-types";
+import { paths as nexus_paths } from "@hiyocord/hiyocord-nexus-types";
 import { AlgorithmName, signRequest } from "../authentication";
 import { createMiddleware } from "hono/factory";
-import createClient from "openapi-fetch";
+import createClient, { ClientOptions } from "openapi-fetch";
+import { getClient, paths as discord_paths } from "@hiyocord/discord-rest-api";
+import { url } from "node:inspector";
 
 export type Env = {
   Bindings: {
@@ -10,12 +12,47 @@ export type Env = {
     HIYOCORD_KEY_ALGORITHM: AlgorithmName
   },
   Variables: {
-    nexusFetch: ReturnType<typeof createNexusFetch>
+    nexus: ReturnType<typeof createNexusFetch>
+    proxy: {
+      discord: ReturnType<typeof createDiscordApiProxy>
+    }
   }
 }
 
+export const createDiscordApiProxy = (url: string, privateKey: string, algorithm: AlgorithmName, options?: ClientOptions) => {
+  const client = createClient<discord_paths>({
+    baseUrl: url,
+    ...options,
+  });
+
+  client.use({
+    onRequest: async ({request}) => {
+      let headers: Record<string, string> = {};
+      request.headers.forEach((v, k) => headers[k] = v)
+      const body = await request.arrayBuffer();
+
+      const signedHeaders = await signRequest(
+        algorithm,
+        privateKey,
+        headers,
+        body
+      )
+
+      return new Request(request.url, {
+        ...request,
+        headers: signedHeaders,
+        body: body
+      })
+    }
+  })
+
+  return client;
+}
+
+
+
 export const createNexusFetch = (url: string, privateKey: string, algorithm: AlgorithmName) => {
-  const client = createClient<paths>({
+  const client = createClient<nexus_paths>({
     baseUrl: url
   })
 
@@ -45,7 +82,10 @@ export const createNexusFetch = (url: string, privateKey: string, algorithm: Alg
 
 export const useNexusFetch = createMiddleware<Env>((c, next) => {
   const nexusFetch = createNexusFetch(c.env.NEXUS_URL, c.env.HIYOCORD_PRIVATE_KEY, c.env.HIYOCORD_KEY_ALGORITHM);
-  c.set("nexusFetch", nexusFetch);
+  c.set("nexus", nexusFetch);
+  c.set("proxy", {
+    discord: createDiscordApiProxy(`${c.env.NEXUS_URL}/api/proxy/discord/v10`, c.env.HIYOCORD_PRIVATE_KEY, c.env.HIYOCORD_KEY_ALGORITHM)
+  });
   return next()
 })
 
